e28439d403c28b4e2c550c8580eda7113c956615
Evan Kuchar <evan.kuchar@lookout.com>
2020-04-08 14:18:29 -0700

    initial commit

---
+++ handler.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const _ = require("lodash");
+const moment = require("moment");
+
+module.exports.sync = async event => {
+  console.log(_);
+  console.log(moment);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
